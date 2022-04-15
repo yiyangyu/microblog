333714903e4d01feb34f238fd51174187e5ea7ac
袁翔 <yuanxiang@inspur.com>
2022-04-15 17:27:34 +0800

    chapter3: web forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
 
 from app import routes  # __init__ 与 routes 相互 import，这一行放在最后而不是行首可以保证 routes 能成功 import app.app
