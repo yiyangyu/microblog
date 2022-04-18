9480d38f28e5f3956fec44a6617447c932f1c56c
袁翔 <yuanxiang@inspur.com>
2022-04-18 14:19:50 +0800

    chapter4: database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes  # __init__ 与 routes 相互 import，这一行放在最后而不是行首可以保证 routes 能成功 import app.app
+from app import routes, models  # __init__ 与 routes 相互 import，这一行放在最后而不是行首可以保证 routes 能成功 import app.app
